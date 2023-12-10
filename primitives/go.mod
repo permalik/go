3c90e10d35c69f4ca02b6ecd9bb129a8494a8fa4
permalik <permalik@protonmail.com>
2023-12-09 18:26:26 -0600

    primitives: binary literals

---
+++ primitives/go.mod
@@ -0,0 +1,3 @@
+module go/literals
+
+go 1.21.5
---
+++ primitives/literals.go
@@ -0,0 +1,29 @@
+package main 
+
+import "fmt"
+
+func main() {
+    binaryZero := 0b0
+    binaryOne := 0b1
+    binaryTwo := 0b10
+    binaryThree := 0b11
+    binaryFour := 0b100
+    binaryFive := 0b101
+    binarySix := 0b110
+    binarySeven := 0b111
+    binaryEight := 0b1000
+    binaryNine := 0b1001
+    binaryTen := 0b1010
+    fmt.Println(binaryZero)
+    fmt.Println(binaryOne)
+    fmt.Println(binaryTwo)
+    fmt.Println(binaryThree)
+    fmt.Println(binaryFour)
+    fmt.Println(binaryFive)
+    fmt.Println(binarySix)
+    fmt.Println(binarySeven)
+    fmt.Println(binaryEight)
+    fmt.Println(binaryNine)
+    fmt.Println(binaryTen)
+}
+
